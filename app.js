37c7d86ac2d2e66dd0d86d59eaecdaff530b0c5a
PIERO FREUNDT <freundtpiero@gmail.com>
2023-04-24 21:46:33 -0500

    .

---
+++ app.js
@@ -0,0 +1,45 @@
+// CREACION DE VARIABLES 
+// var, const y let 
+
+let name = 'PIERO';
+let age = 28;
+let logged = true;
+let notLogged = false;
+let lastName = null;
+let secondNAME;
+
+console.log(typeof name);
+console.log(typeof age);
+
+let proxSchool;
+let nameSON;
+let nameDAUGHTER;
+
+let name = "Piero";
+let address = "jr. Los Papiros #258";
+let email = "freundtpiero@gmail.com";
+
+let age = 24;
+let phone = 555 - 1234;
+let postalCode = 12345;
+
+let isMarried = true;
+let isFemale = false;
+let isOver18 = true;
+
+let contactInfo = {
+    email: "freundtpiero@gmail.com",
+    phone: 555 - 1234,
+    address: "jr. Los Papiros #258"
+};
+
+let jobInfo = {
+    title: "Software Developer",
+    company: "ABC Inc.",
+    salary: 5000
+};
+
+
+let interests = ["Programming", "Music", "Travel"];
+let skills = ["JavaScript", "HTML", "CSS", "Python"];
+let hobbies = ["Reading", "Playing video games", "Cooking"];
